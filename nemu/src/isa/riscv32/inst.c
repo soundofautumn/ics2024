7534ad7d7fcba65416909d87604d89cc5cc85458
tracer-ics2024 <tracer@njuics.org>
2026-04-25 18:10:13 +0800

    >  compile NEMU 231220000 张三 Linux DESKTOP-B17TEJJ 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  18:10:13 up  7:34,  1 user,  load average: 0.09, 0.19, 0.13

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -146,7 +146,7 @@ static int decode_exec(Decode *s) {
   INSTPAT("0000001 ????? ????? 111 ????? 01100 11", remu   , R, R(rd) = (src2 == 0) ? src1 : (src1 % src2));
 
   // system
-  INSTPAT("0000000 00000 00000 000 00000 11100 11", ecall  , N, mepc = s->pc; mcause = 11; s->dnpc = mtvec; );
+  INSTPAT("0000000 00000 00000 000 00000 11100 11", ecall  , N, isa_raise_intr(0, s->pc); );
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
 
   // RV32/RV64 Zicsr Standard Extension
@@ -159,7 +159,7 @@ static int decode_exec(Decode *s) {
   INSTPAT("??????? ????? ????? ??? ????? 01101 11", lui    , U, R(rd) = imm);
 
   // privileged instructions
-  INSTPAT("0011000 00010 00000 000 00000 11100 11", mret   , N, mepc = s->pc; mcause = 3; s->dnpc = mtvec; );
+  INSTPAT("0011000 00010 00000 000 00000 11100 11", mret   , N, s->dnpc = mtvec; );
 
   INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv    , N, INV(s->pc));
   INSTPAT_END();
--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -17,9 +17,6 @@
 #include "../local-include/reg.h"
 
 word_t isa_raise_intr(word_t NO, vaddr_t epc) {
-  /* TODO: Trigger an interrupt/exception with ``NO''.
-   * Then return the address of the interrupt/exception vector.
-   */
   mepc = epc;  // mepc
   mcause = NO;  // mcause
   return mtvec; // mtvec
