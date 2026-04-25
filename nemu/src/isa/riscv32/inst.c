ade669eb77f1fab073fd44f0a1ccb1422dc875f5
tracer-ics2024 <tracer@njuics.org>
2026-04-25 14:20:13 +0800

    >  compile NEMU 231220000 张三 Linux DESKTOP-B17TEJJ 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:20:13 up  3:44,  1 user,  load average: 0.24, 0.18, 0.17

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -93,7 +93,7 @@ static int decode_exec(Decode *s) {
   // jalr
   IFDEF(CONFIG_FTRACE, void ftrace_call(word_t pc, word_t target_addr); void ftrace_ret(word_t pc););
 
-  INSTPAT("??????? ????? ????? 000 ????? 11001 11", jalr   , I, R(rd) = s->snpc; s->dnpc = (src1 + imm) & ~1; IFDEF(CONFIG_FTRACE) if (rd != 0) ftrace_ret(s->pc); else ftrace_call(s->pc, s->dnpc););
+  INSTPAT("??????? ????? ????? 000 ????? 11001 11", jalr   , I, R(rd) = s->snpc; s->dnpc = (src1 + imm) & ~1; IFDEF(CONFIG_FTRACE) if (rd != 0 && src1 == 1 && src2 == 0) ftrace_call(s->pc, s->dnpc); else ftrace_ret(s->pc););
 
   // 011
   // jal
