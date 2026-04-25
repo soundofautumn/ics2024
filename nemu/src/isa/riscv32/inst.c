87e9cdb7aee00de6e8aef42c7293d925ffc6aaf0
tracer-ics2024 <tracer@njuics.org>
2026-04-25 13:41:30 +0800

    >  compile NEMU 231220000 张三 Linux DESKTOP-B17TEJJ 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:41:30 up  3:05,  1 user,  load average: 0.15, 0.12, 0.12

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -47,6 +47,7 @@ static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_
     case TYPE_I:       src1R();          immI();       break;
     case TYPE_I_shamt: src1R();          immI_shamt(); break;
     case TYPE_S:       src1R(); src2R(); immS();       break;
+    case TYPE_B:       src1R(); src2R(); immB();       break;
     case TYPE_U:                         immU();       break;
     case TYPE_J:                         immJ();       break;
     case TYPE_N: break;
