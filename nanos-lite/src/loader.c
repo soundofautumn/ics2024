269a0d3e93a5d31e7a0d1672b04cc2dd9f15441b
tracer-ics2024 <tracer@njuics.org>
2026-04-26 17:14:59 +0800

    >  compile NEMU 231220000 张三 Linux DESKTOP-B17TEJJ 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:14:59 up  6:51,  1 user,  load average: 0.05, 0.11, 0.07

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -9,9 +9,21 @@
 # define Elf_Phdr Elf32_Phdr
 #endif
 
+size_t get_ramdisk_size();
+size_t ramdisk_read(void *buf, size_t offset, size_t len);
+
+
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();
-  return 0;
+  size_t elf_size = get_ramdisk_size();
+  char *elf_buf = (char *)malloc(elf_size);
+  assert(elf_buf != NULL);
+  assert(ramdisk_read(elf_buf, 0, elf_size) == elf_size);
+
+  Elf_Ehdr *ehdr = (Elf_Ehdr *)elf_buf;
+  assert(memcmp(ehdr->e_ident, ELFMAG, SELFMAG) == 0);
+  assert(ehdr->e_type == ET_EXEC);
+  assert(ehdr->e_phnum > 0);
+  return ehdr->e_entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
