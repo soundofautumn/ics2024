576d6a12d6d23ce9b9f8ca11d62fdf622ed8232a
tracer-ics2024 <tracer@njuics.org>
2026-04-26 20:46:32 +0800

    >  compile NEMU 231220000 张三 Linux DESKTOP-B17TEJJ 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:46:32 up 10:22,  1 user,  load average: 0.12, 0.11, 0.09

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -4,6 +4,8 @@ void do_syscall(Context *c);
 
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
+    case EVENT_YIELD:
+      Log("Nanos in yield"); break;
     case EVENT_SYSCALL: {
       do_syscall(c);
       break;
