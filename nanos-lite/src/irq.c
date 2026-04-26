bf4d0fce4afd25c6e452c2ef2a5d71d9130d7c98
tracer-ics2024 <tracer@njuics.org>
2026-04-26 19:53:14 +0800

    >  compile NEMU 231220000 张三 Linux DESKTOP-B17TEJJ 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:53:14 up  9:28,  1 user,  load average: 0.12, 0.07, 0.09

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,8 +1,14 @@
 #include <common.h>
 
+
+intptr_t do_syscall(intptr_t type, intptr_t a0, intptr_t a1, intptr_t a2);
+
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
-    case EVENT_YIELD: yield(); break;
+    case EVENT_SYSCALL: {
+      c->GPRx = do_syscall(c->GPR1, c->GPR2, c->GPR3, c->GPR4);
+      break;
+    }
     default: panic("Unhandled event ID = %d", e.event);
   }
 
