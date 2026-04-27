df91bda1e78baebb8afca7f948d26805c46912b3
tracer-ics2024 <tracer@njuics.org>
2026-04-27 19:53:00 +0800

    >  compile NEMU 231220000 张三 Linux DESKTOP-B17TEJJ 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:53:00 up  1:16,  1 user,  load average: 0.13, 0.13, 0.09

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -3,12 +3,7 @@
 void do_syscall(Context *c);
 
 static Context* do_event(Event e, Context* c) {
-  // Log("Event: %d", e.event);
   switch (e.event) {
-    case EVENT_YIELD: {
-      // Log("Nanos in yield"); 
-      break;
-    }
     case EVENT_SYSCALL: {
       do_syscall(c);
       break;
--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,22 +1,29 @@
 #include <common.h>
 #include "syscall.h"
 
-static void sys_yield(Context *c) {
+static void sys_yield(uintptr_t *a) {
   yield();
-  c->GPR2 = 0;
+  a[2] = 0;
 }
 
-static void sys_exit(Context *c) {
-  halt(c->GPR2);
+static void sys_exit(uintptr_t *a) {
+  halt(a[2]);
 }
 
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
+  a[1] = c->GPR2;
+  a[2] = c->GPR3;
+  a[3] = c->GPR4;
 
   switch (a[0]) {
-    case SYS_yield: sys_yield(c); break;
-    case SYS_exit: sys_exit(c); break;
+    case SYS_yield: sys_yield(a); break;
+    case SYS_exit: sys_exit(a); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
+
+#ifdef STRACE
+    Log("strace: syscall %d => %d", a[0], a[2]);
+#endif
 }
