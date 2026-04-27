93d97d61eaec9a2dcf9abea4b1dc7875a2c406d5
tracer-ics2024 <tracer@njuics.org>
2026-04-27 21:55:22 +0800

    >  compile NEMU 231220000 张三 Linux DESKTOP-B17TEJJ 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:55:22 up  3:18,  1 user,  load average: 0.25, 0.13, 0.11

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -3,6 +3,19 @@
 
 // #define CONFIG_STRACE
 
+#ifdef CONFIG_STRACE
+char strace_buf[128];
+#define STRACE_LOG(...) do { \
+  int len = snprintf(strace_buf, sizeof(strace_buf), __VA_ARGS__); \
+  strace_buf[len] = '\0'; \
+  for (int i = 0; i < len; i++) { \
+    putch(strace_buf[i]); \
+  } \
+} while (0)
+#else // CONFIG_STRACE
+#define STRACE_LOG(...) do { } while (0)
+#endif
+
 void do_syscall(Context *c) {
   uintptr_t sysnum = c->GPR1;
   uintptr_t a0 = c->GPR2;
@@ -10,19 +23,15 @@ void do_syscall(Context *c) {
   uintptr_t a2 = c->GPR4;
   uintptr_t ret = 0;
 
-#ifdef CONFIG_STRACE
-  if (sysnum == SYS_exit) {
-    Log("syscall: exit(%d)", a0);
-  }
-#endif
-
   switch (sysnum) {
     case SYS_yield: {
       yield();
       ret = 0;
+      STRACE_LOG("syscall: yield() -> %d", ret);
       break;
     }
     case SYS_exit: {
+      STRACE_LOG("syscall: exit(%d)", a0);
       halt(a0);
       break;
     }
@@ -32,18 +41,11 @@ void do_syscall(Context *c) {
         putch(buf[i]);
       }
       ret = a2;
+      STRACE_LOG("syscall: write(%d) -> %d", a2, ret);
       break;
     }
     default: panic("Unhandled syscall ID = %d", sysnum);
   }
-
-#ifdef CONFIG_STRACE
-  switch (sysnum) {
-    case SYS_yield: Log("syscall: yield() -> %d", ret); break;
-    case SYS_write: Log("syscall: write(%d) -> %d", a2, ret); break;
-    default: panic("Unhandled syscall ID = %d", sysnum);
-  }
-#endif
 
   c->GPRx = ret;
 }
