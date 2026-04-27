ef0698a659f903e917b5b7ebdd0d2483ab9bd7cb
tracer-ics2024 <tracer@njuics.org>
2026-04-27 20:03:38 +0800

    >  compile NEMU 231220000 张三 Linux DESKTOP-B17TEJJ 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:03:38 up  1:26,  1 user,  load average: 0.26, 0.13, 0.10

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -23,6 +23,7 @@ void do_syscall(Context *c) {
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
+#define CONFIG_STRACE
 #ifdef CONFIG_STRACE
     Log("strace: syscall %d => %d", a[0], a[2]);
 #endif
