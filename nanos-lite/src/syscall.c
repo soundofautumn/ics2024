0e95698f246a5d5e7146ac096857f13feaeee5bb
tracer-ics2024 <tracer@njuics.org>
2026-04-27 21:04:11 +0800

    >  compile NEMU 231220000 张三 Linux DESKTOP-B17TEJJ 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:04:11 up  2:27,  1 user,  load average: 0.41, 0.23, 0.18

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -12,6 +12,14 @@ static void sys_exit(uintptr_t *a) {
   halt(a[2]);
 }
 
+static void sys_write(uintptr_t *a) {
+  char *buf = (char *)a[2];
+  for (int i = 0; i < a[3]; i++) {
+    putch(buf[i]);
+  }
+  a[2] = a[3];
+}
+
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
@@ -29,13 +37,17 @@ void do_syscall(Context *c) {
   switch (a[0]) {
     case SYS_yield: sys_yield(a); break;
     case SYS_exit: sys_exit(a); break;
+    case SYS_write: sys_write(a); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
 #ifdef CONFIG_STRACE
   switch (a[0]) {
     case SYS_yield: Log("syscall: yield() -> %d", a[2]); break;
+    case SYS_write: Log("syscall: write(%d) -> %d", a[3], a[2]); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 #endif
+
+  c->GPRx = a[2];
 }
