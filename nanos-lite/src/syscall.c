c80ae95c50e4e467a5f45c7f888824084855a2a9
tracer-ics2024 <tracer@njuics.org>
2026-04-28 13:44:15 +0800

    >  compile NEMU 231220000 张三 Linux DESKTOP-B17TEJJ 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:44:15 up  2:12,  1 user,  load average: 0.08, 0.08, 0.09

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -44,7 +44,7 @@ void do_syscall(Context *c) {
     }
     case SYS_brk: {
       ret = 0;
-      STRACE_LOG("syscall: brk(%d) -> %d", a0, ret);
+      STRACE_LOG("syscall: brk(%p) -> %d", (void *)a0, ret);
       break;
     }
     case SYS_open: {
