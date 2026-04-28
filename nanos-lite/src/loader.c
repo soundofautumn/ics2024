1c531fea83803c3b59e21bfcb3c9b97bff317b87
tracer-ics2024 <tracer@njuics.org>
2026-04-28 13:00:46 +0800

    >  compile NEMU 231220000 张三 Linux DESKTOP-B17TEJJ 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:00:46 up  1:29,  1 user,  load average: 0.06, 0.09, 0.09

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -45,6 +45,8 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
         // [VirtAddr + FileSiz, VirtAddr + MemSiz)
         memset((void *)(phdr.p_vaddr + phdr.p_filesz), 0, phdr.p_memsz - phdr.p_filesz);
       }
+      Log("phdr[%d]: p_offset = 0x%08x, p_vaddr = 0x%08x, p_filesz = 0x%08x, p_memsz = 0x%08x",
+          i, phdr.p_offset, phdr.p_vaddr, phdr.p_filesz, phdr.p_memsz);
     }
   }
   fs_close(fd);
