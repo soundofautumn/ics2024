32dc3e61824eec55f5f8f897c95306f3f9883515
tracer-ics2024 <tracer@njuics.org>
2026-04-28 18:06:45 +0800

    >  compile NEMU 231220000 张三 Linux DESKTOP-B17TEJJ 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  18:06:45 up  6:35,  1 user,  load average: 0.46, 0.19, 0.10

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -22,7 +22,11 @@ uint32_t NDL_GetTicks() {
 }
 
 int NDL_PollEvent(char *buf, int len) {
-  return 0;
+  if (evtdev < 0) return 0;
+  int nread = read(evtdev, buf, len - 1);
+  if (nread <= 0) return 0;
+  buf[nread] = '\0';
+  return nread;
 }
 
 void NDL_OpenCanvas(int *w, int *h) {
@@ -67,6 +71,7 @@ int NDL_Init(uint32_t flags) {
     evtdev = 3;
   }
   gettimeofday(&start_time, NULL);
+  eventfd = open("/dev/events", 0, 0);
   return 0;
 }
 
