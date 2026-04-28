910b72ae7ea5f4b5a53b8e28cd76e4157a0cdea9
tracer-ics2024 <tracer@njuics.org>
2026-04-28 20:13:34 +0800

    >  compile NEMU 231220000 张三 Linux DESKTOP-B17TEJJ 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:13:34 up  8:42,  1 user,  load average: 0.24, 0.23, 0.13

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -46,11 +46,10 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
 }
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
-  int x = offset % io_read(AM_GPU_CONFIG).width;
-  int y = offset / io_read(AM_GPU_CONFIG).width;
-  int w = len;
-  int h = 1;
-  io_write(AM_GPU_FBDRAW, x, y, (void *)buf, w, h, true);
+  int w = io_read(AM_GPU_CONFIG).width;
+  int x = offset % w;
+  int y = offset / w;
+  io_write(AM_GPU_FBDRAW, x, y, (void *)buf, len, 1, true);
   return 0;
 }
 
