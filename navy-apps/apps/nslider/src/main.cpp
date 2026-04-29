d64123576afb79e7347338a77499a63361af42e9
tracer-ics2024 <tracer@njuics.org>
2026-04-29 11:29:47 +0800

    >  compile NEMU 231220000 张三 Linux DESKTOP-B17TEJJ 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  11:29:47 up 32 min,  1 user,  load average: 0.56, 0.55, 0.34

--- navy-apps/apps/nslider/src/main.cpp
+++ navy-apps/apps/nslider/src/main.cpp
@@ -56,6 +56,7 @@ int main() {
     SDL_WaitEvent(&e);
 
     if (e.type == SDL_KEYDOWN) {
+      printf("key down: %d\n", e.key.keysym.sym);
       switch(e.key.keysym.sym) {
         case SDLK_0: rep = rep * 10 + 0; break;
         case SDLK_1: rep = rep * 10 + 1; break;
