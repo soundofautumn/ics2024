47ba73fa4daff4aee3afa143ce8bc44e2be614d2
tracer-ics2024 <tracer@njuics.org>
2026-04-29 19:50:30 +0800

    >  compile NEMU 231220000 张三 Linux DESKTOP-B17TEJJ 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:50:30 up  8:53,  1 user,  load average: 0.23, 0.35, 0.37

--- navy-apps/apps/menu/menu.cpp
+++ navy-apps/apps/menu/menu.cpp
@@ -27,8 +27,8 @@ struct MenuItem {
 } items[] = {
   {"NJU Terminal", "/bin/nterm", NULL},
   {"NSlider", "/bin/nslider", NULL},
-  {"FCEUX (Super Mario Bros)", "/bin/fceux", "/share/games/nes/mario.nes"},
-  {"FCEUX (100 in 1)", "/bin/fceux", "/share/games/nes/100in1.nes"},
+  {"FCEUX (Super Mario Bros)", "/bin/fceux", "/share/games/nes/SuperMarioBros.nes"},
+  // {"FCEUX (100 in 1)", "/bin/fceux", "/share/games/nes/100in1.nes"},
   {"Flappy Bird", "/bin/bird", NULL},
   {"PAL - Xian Jian Qi Xia Zhuan", "/bin/pal", NULL},
   {"NPlayer", "/bin/nplayer", NULL},
