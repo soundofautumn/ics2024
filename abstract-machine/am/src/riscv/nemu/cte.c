bab15c2c6043a9b15776bca6b31b15674304e1d2
tracer-ics2024 <tracer@njuics.org>
2026-05-03 14:49:31 +0800

    >  compile NEMU 231220000 张三 Linux DESKTOP-B17TEJJ 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:49:31 up 40 min,  1 user,  load average: 0.21, 0.51, 0.41

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -22,7 +22,6 @@ Context* __am_irq_handle(Context *c) {
     c = user_handler(ev, c);
     assert(c != NULL);
   }
-  c->mepc += 4;
   return c;
 }
 
@@ -39,7 +38,13 @@ bool cte_init(Context*(*handler)(Event, Context*)) {
 }
 
 Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
-  return NULL;
+  Context *ctx = (Context *)(kstack.end - sizeof(Context));
+  *ctx = (Context){0};
+  ctx->mepc = (uintptr_t)entry;
+  ctx->mstatus = (3 << 11);
+  ctx->gpr[2] = (uintptr_t)kstack.end - (NR_REGS + 3) * sizeof(uintptr_t);
+  ctx->gpr[10] = (uintptr_t)arg;
+  return ctx;
 }
 
 void yield() {
--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -5,10 +5,12 @@ void do_syscall(Context *c);
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
     case EVENT_YIELD: {
+      c->mepc += 4;
       break;
     }
     case EVENT_SYSCALL: {
       do_syscall(c);
+      c->mepc += 4;
       break;
     }
     default: panic("Unhandled event ID = %d", e.event);
