d612663181ea32216068c24df3f1ccfd12cb22e6
tracer-ics2024 <tracer@njuics.org>
2026-05-03 20:36:47 +0800

    >  compile NEMU 231220000 张三 Linux DESKTOP-B17TEJJ 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:36:47 up  6:27,  1 user,  load average: 0.39, 0.11, 0.05

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,10 +1,12 @@
 #include <common.h>
 
 void do_syscall(Context *c);
+Context* schedule(Context *prev);
 
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
     case EVENT_YIELD: {
+      schedule(c);
       break;
     }
     case EVENT_SYSCALL: {
--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -21,7 +21,13 @@ void hello_fun(void *arg) {
   }
 }
 
+void context_kload(PCB *pcb, void (*entry)(void *), void *arg) {
+  pcb->cp = kcontext((Area) { pcb->stack, pcb->stack + STACK_SIZE }, entry, arg);
+}
+
 void init_proc() {
+  context_kload(&pcb[0], hello_fun, (void *)0xdeadbeef);
+  context_kload(&pcb[1], hello_fun, (void *)0x12345678);
   switch_boot_pcb();
 
   Log("Initializing processes...");
