1ed09b132755d1cf5cedd94518ddd5c3a2a25b6e
tracer-ics2024 <tracer@njuics.org>
2026-05-04 12:33:14 +0800

    >  compile NEMU 231220000 张三 Linux DESKTOP-B17TEJJ 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  12:33:14 up  1:38,  1 user,  load average: 1.13, 0.70, 0.37

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -1,22 +1,22 @@
 #include <stdint.h>
 #include <stdlib.h>
-#include <assert.h>
 #include <stdio.h>
+#include <assert.h>
 
 int main(int argc, char *argv[], char *envp[]);
 extern char **environ;
 void call_main(uintptr_t *args) {
   int argc = (int)args[0];
   printf("argc = %d\n", argc);
-  char **argv = (char **)&args[1];
-  for (int i = 0; i < argc; i ++) {
-    printf("argv[%d] = %s\n", i, argv[i]);
-  }
-  char **envp = (char **)&args[argc + 2];
-  for (int i = 0; envp[i] != NULL; i ++) {
-    printf("envp[%d] = %s\n", i, envp[i]);
-  }
-  environ = envp;
-  exit(main(argc, argv, envp));
+  // char **argv = (char **)&args[1];
+  // for (int i = 0; i < argc; i ++) {
+  //   printf("argv[%d] = %s\n", i, argv[i]);
+  // }
+  // char **envp = (char **)&args[argc + 2];
+  // for (int i = 0; envp[i] != NULL; i ++) {
+  //   printf("envp[%d] = %s\n", i, envp[i]);
+  // }
+  // environ = envp;
+  // exit(main(argc, argv, envp));
   assert(0);
 }
