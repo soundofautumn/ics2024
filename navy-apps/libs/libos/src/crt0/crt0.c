d4d30daaf407a28e8d5407607eb1a2a248305d3e
tracer-ics2024 <tracer@njuics.org>
2026-05-04 12:26:32 +0800

    >  compile NEMU 231220000 张三 Linux DESKTOP-B17TEJJ 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  12:26:32 up  1:32,  1 user,  load average: 0.54, 0.29, 0.16

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -5,6 +5,7 @@
 int main(int argc, char *argv[], char *envp[]);
 extern char **environ;
 void call_main(uintptr_t *args) {
+  printf("call_main args: %p %p %p\n", (void *)args[0], (void *)args[1], (void *)args[2]);
   int argc = (int)args[0];
   char **argv = (char **)&args[1];
   char **envp = (char **)&args[argc + 2];
