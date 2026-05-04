e2c3614457d04d82a6b5c8b695bc3edaa1113b40
tracer-ics2024 <tracer@njuics.org>
2026-05-04 12:51:59 +0800

    >  compile NEMU 231220000 张三 Linux DESKTOP-B17TEJJ 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  12:51:59 up  1:57,  1 user,  load average: 0.79, 0.58, 0.41

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -31,7 +31,7 @@ void init_proc() {
   // char *const argv[] = { "/bin/menu", NULL };
   // char *const envp[] = { NULL };
   // context_uload(&pcb[1], "/bin/menu", argv, envp);
-  context_uload(&pcb[1], "/bin/menu", NULL, NULL);
+  context_uload(&pcb[1], "/bin/hello", NULL, NULL);
   switch_boot_pcb();
 
   Log("Initializing processes...");
