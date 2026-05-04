125c804c3c1434e35b56196043b521e7d7d484b0
tracer-ics2024 <tracer@njuics.org>
2026-05-04 12:53:51 +0800

    >  compile NEMU 231220000 张三 Linux DESKTOP-B17TEJJ 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  12:53:51 up  1:59,  1 user,  load average: 0.72, 0.57, 0.42

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -44,6 +44,7 @@ Context* schedule(Context *prev) {
   for (int i = 0; i < MAX_NR_PROC; i ++) {
     if (pcb[i].cp != NULL && pcb[i].cp != prev) {
       current = &pcb[i];
+      printf("Switching to PCB %d\n", i);
       return current->cp;
     }
   }
