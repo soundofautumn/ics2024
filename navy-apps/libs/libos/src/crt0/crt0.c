9cee51a9cd7335b66427991494b8dbefbccc7939
tracer-ics2024 <tracer@njuics.org>
2026-05-04 12:31:23 +0800

    >  compile NEMU 231220000 张三 Linux DESKTOP-B17TEJJ 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  12:31:23 up  1:36,  1 user,  load average: 0.40, 0.38, 0.23

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -8,6 +8,13 @@ void call_main(uintptr_t *args) {
   int argc = (int)args[0];
   char **argv = (char **)&args[1];
   char **envp = (char **)&args[argc + 2];
+  printf("argc = %d\n", argc);
+  for (int i = 0; i < argc; i ++) {
+    printf("argv[%d] = %s\n", i, argv[i]);
+  }
+  for (int i = 0; envp[i] != NULL; i ++) {
+    printf("envp[%d] = %s\n", i, envp[i]);
+  }
   environ = envp;
   exit(main(argc, argv, envp));
   assert(0);
