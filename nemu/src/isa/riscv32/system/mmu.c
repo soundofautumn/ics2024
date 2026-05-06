7c5ff8f76b1d5c7f395820fd651d0b9301d5584c
tracer-ics2024 <tracer@njuics.org>
2026-05-06 21:37:42 +0800

    >  compile NEMU 231220000 张三 Linux DESKTOP-B17TEJJ 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:37:42 up 19 min,  1 user,  load average: 0.23, 0.15, 0.10

--- nemu/src/isa/riscv32/system/mmu.c
+++ nemu/src/isa/riscv32/system/mmu.c
@@ -41,7 +41,7 @@ paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
   pte = paddr_read(pte_addr, sizeof(pte));
   if (!PTE_V(pte)) return MEM_RET_FAIL;
 
-  pdir = PTE_PPN1(pte) << PAGE_SHIFT;
+  pdir = PTE_PPN(pte) << PAGE_SHIFT;
   pte_addr = pdir + VPN0(vaddr) * sizeof(pte);
   Log("satp = " FMT_WORD ", pdir = " FMT_PADDR ", pte_addr = " FMT_PADDR, satp, pdir, pte_addr);
   pte = paddr_read(pte_addr, sizeof(pte));
