42a602318fb0efaa475929111618316985e08cc0
tracer-ics2024 <tracer@njuics.org>
2026-05-07 21:34:10 +0800

    >  compile NEMU 231220000 张三 Linux DESKTOP-B17TEJJ 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:34:10 up  1:05,  1 user,  load average: 0.19, 0.22, 0.14

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -62,6 +62,7 @@ void __am_get_cur_as(Context *c) {
 
 void __am_switch(Context *c) {
   if (vme_enable && c->pdir != NULL) {
+    printf("switch to address space %p\n", c->pdir);
     set_satp(c->pdir);
   }
 }
