2e3ac194e72eb05d623e9d233d5f3d0d49ec0a18
tracer-ics2024 <tracer@njuics.org>
2026-05-07 21:50:11 +0800

    >  compile NEMU 231220000 张三 Linux DESKTOP-B17TEJJ 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:50:11 up  1:21,  1 user,  load average: 1.24, 0.59, 0.29

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -83,6 +83,7 @@ void map(AddrSpace *as, void *va, void *pa, int prot) {
   if (!(pte & PTE_V)) {
     l0table = (PTE *)pgalloc_usr(PGSIZE);
     updir[vpn1] = (((uintptr_t)l0table >> 12) << 10) | PTE_V;
+    printf("new page for vpn1 %d at %p\n", vpn1, l0table);
   } else {
     l0table = (PTE *)(PTE)(PTE_PPN(pte) << 12);
   }
