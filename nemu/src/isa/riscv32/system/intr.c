d1d75553d28c983ab9ce5428ebbea407cc95be04
tracer-ics2024 <tracer@njuics.org>
2026-05-08 23:09:52 +0800

    >  compile NEMU 231220000 张三 Linux DESKTOP-B17TEJJ 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  23:09:52 up  5:38,  1 user,  load average: 0.29, 0.34, 0.22

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -27,7 +27,7 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
 }
 
 word_t isa_query_intr() {
-  if (cpu.INTR) {
+  if ((mstatus & MIE) && cpu.INTR) {
     cpu.INTR = false;
     return IRQ_TIMER;
   }
