93a4de3ed53e3796a15fbc1451ca914a17b7ffe8
tracer-ics2024 <tracer@njuics.org>
2026-05-09 21:24:31 +0800

    >  compile NEMU 231220000 张三 Linux DESKTOP-B17TEJJ 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:24:31 up 10:24,  1 user,  load average: 0.28, 0.38, 0.53

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -6,7 +6,7 @@ Context* schedule(Context *prev);
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
     case EVENT_IRQ_TIMER:
-      // Log("Timer interrupt triggered");
+      Log("Timer interrupt triggered");
     case EVENT_YIELD: {
       return schedule(c);
     }
