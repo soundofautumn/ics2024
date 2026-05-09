21293afb9d5af03da5c5d2b9c252a70c0241f90e
tracer-ics2024 <tracer@njuics.org>
2026-05-09 15:53:59 +0800

    >  compile NEMU 231220000 张三 Linux DESKTOP-B17TEJJ 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:53:59 up  4:54,  1 user,  load average: 0.23, 0.11, 0.09

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -154,7 +154,8 @@ static int decode_exec(Decode *s) {
   INSTPAT("0000001 ????? ????? 111 ????? 01100 11", remu   , R, R(rd) = (src2 == 0) ? src1 : (src1 % src2));
 
   // system
-  INSTPAT("0000000 00000 00000 000 00000 11100 11", ecall  , N, s->dnpc = isa_raise_intr(ECALL_M, s->pc); IFDEF(CONFIG_ETRACE, Log("ecall with NO = %d, pc = 0x%x", GPR1, s->pc)); );
+  INSTPAT("0000000 00000 00000 000 00000 11100 11", ecall  , N, s->dnpc = isa_raise_intr(ECALL_M, s->pc); 
+                                                                IFDEF(CONFIG_ETRACE, Log("ecall with NO = %d, pc = 0x%x", GPR1, s->pc)); );
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
 
   // RV32/RV64 Zicsr Standard Extension
