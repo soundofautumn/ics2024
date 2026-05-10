436d806901e25ab3659fbedf5428b763f6f2ea95
tracer-ics2024 <tracer@njuics.org>
2026-05-10 15:23:05 +0800

    >  compile NEMU 231220000 张三 Linux DESKTOP-B17TEJJ 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:23:05 up  2:09,  1 user,  load average: 0.31, 0.41, 0.29

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -9,6 +9,9 @@ void __am_get_cur_as(Context *c);
 void __am_switch(Context *c);
 
 Context* __am_irq_handle(Context *c) {
+  printf("Handling interrupt: mcause = 0x%x, mepc = 0x%x\n", c->mcause, c->mepc);
+  printf("Context dump: gpr[0] = 0x%x, gpr[1] = 0x%x, gpr[2] = 0x%x, gpr[3] = 0x%x\n",
+         c->gpr[0], c->gpr[1], c->gpr[2], c->gpr[3]);
   __am_get_cur_as(c);
   if (user_handler) {
     Event ev = {0};
